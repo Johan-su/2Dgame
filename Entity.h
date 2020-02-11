432863dffa7c0bb66d5998297fa59241d33dd205
Johan Sundin <02JOS004@lulea.se>
2020-02-11 18:25:04 +0100

    fixed adding classes

---
+++ Entity.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "SDL.h"
+
+class Entity {
+public:
+private:
+	float x;
+	float y;
+	float height;
+	float width;
+public:
+private:
+};
---
+++ Player.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "SDL.h"
+#include <iostream>
+class Player {
+public:
+	Player();
+	~Player();
+};
